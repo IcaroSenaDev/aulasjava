169ee6003c0074fbb5e6d4b4c4b8697d2da0762f
Icaro Sena Dev <121191541+IcaroSenaDev@users.noreply.github.com>
2024-02-05 17:14:26 -0300

    Update treino1Carro.java
    Colocando em prática algumas coisas que eu aprendi nas aulas de POO no senai e alguns vídeos no youtube

--- treino1Carro.java
+++ treino1Carro.java
@@ -35,10 +35,15 @@ class Carro {
         System.out.println("Brand of Car: " + car.brand); //Deverá imprimir Porshe
         System.out.println("Model of Car: " + car.model); //Deverá imprimir 911
         System.out.println("Year of manufacture: " + car.yearofmanufacture); //Deverá imprimir 2023
-        System.out.println("Speed of Car: " + car.speedCar); //Deverar Imprimir 250
+        
+        car.BrakeCar(50); // COMANDO PARA O CARRO FREIAR
+        System.out.println("Speed of Car: " + car.speedCar + " KM/H"); //Deverar Imprimir 200
 
+        car.SpeedUpCar(100); // COMANDO PARA O CARRO ACELERAR
+        System.out.println("Speed of Car: " + car.speedCar + " KM/H"); // DEVERAR IMPRIMIR 300
+
+        car.TurnOnCar(); //Deverá Imprimir CARRO LIGADO
         car.TurnOffCar(); //Deverá Imprimir CARRO DESLIGADO
-        car.BrakeCar(20);
         System.out.println(car.HonkCar()); //DEVERÁ IMPRIMIR BI BI!
     }
 }
