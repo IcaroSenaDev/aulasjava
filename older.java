06d02e9856c91af2808df7b275faf04d8d85518a
Icaro Sena Dev <121191541+IcaroSenaDev@users.noreply.github.com>
2024-02-07 13:53:41 -0300

    Create older.java
    Atividade para o programa identificar se o usuário é maior ou menor de idade

---
+++ older.java
@@ -0,0 +1,23 @@
+package maiordeidade;
+
+import java.util.*;
+
+public class older {
+
+	public static void main(String[] args) {
+		
+		Scanner age = new Scanner(System.in);
+		int maiorIdade = 0;
+		
+		System.out.println("How old are you?"); //PERGUNTANDO A IDADE
+		maiorIdade = age.nextInt();// UM SCANF DA VIDA
+		
+		if (maiorIdade < 18) {
+			System.out.println("You are underare"); //DEVERÁ IMPRIMIR QUE É MENOR DE IDADE
+		}else if(maiorIdade >= 18) {
+			System.out.println("You are of legal age"); // DEVERÁ IMPRIMIR QUE É MAIOR DE IDADE
+		}
+
+	}
+
+}
