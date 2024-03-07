019aabf1ee2e7016dec3b1a0651eb110fac6d94b
Icaro Sena Dev <121191541+IcaroSenaDev@users.noreply.github.com>
2024-03-07 14:11:40 -0300

    Create nomeslistas.java

---
+++ nomeslistas.java
@@ -0,0 +1,23 @@
+package listadeatividadesenai;
+import java.util.ArrayList;
+
+public class Nome {
+	
+	public static void main(String[] args) {
+		ArrayList <String> detentos = new ArrayList <>();
+		
+		detentos.add("VITOR");
+		detentos.add("SAMUEL");
+		detentos.add("PEDRO");
+		
+		System.out.println(detentos);
+		
+		detentos.remove(2);
+		
+		System.out.println(detentos);
+		
+		detentos.add("YURI");
+		
+		System.out.println(detentos);
+	}
+}
